c5daf4d02a9ba3cfd33152eb720b2727aedd7f50
ylor <2200609+ylor@users.noreply.github.com>
2019-05-22 15:45:39 -0400

    Add files via upload

--- favicon.js
+++ favicon.js
@@ -1,16 +1,14 @@
 const favicon = document.querySelector("link[rel=icon]");
 
 if (favicon) {
-  const emoji = favicon.getAttribute("data-emoji");
-
-  if (emoji) {
+ {
     const canvas = document.createElement("canvas");
     canvas.height = 64;
     canvas.width = 64;
 
     const ctx = canvas.getContext("2d");
     ctx.font = "64px serif";
-    ctx.fillText(emoji, 0, 56);
+    ctx.fillText("😋", -10, 56);
 
     favicon.href = canvas.toDataURL();
   }
