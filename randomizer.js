8194da8fa756ede51286278bc65089de789df0a5
ylor <rolyreyes@me.com>
2019-05-22 12:52:06 -0400

    asdf

---
+++ randomizer.js
@@ -0,0 +1,45 @@
+var array = [
+  ["🍖", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍗", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🥩", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🥓", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍔", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍟", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍕", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🌭", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🥪", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🌮", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🌯", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🥙", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍳", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🥘", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍲", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🥣", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🥗", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍿", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍱", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍘", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍙", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍚", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍛", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍜", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍝", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍣", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🍤", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🥟", "11625 N Kendall Dr, Miami, FL 33176"],
+  ["🥡", "11625 N Kendall Dr, Miami, FL 33176"]
+];
+
+var randomRestaurant = array[(Math.random() * array.length) | 0];
+
+function buttonFunction() {
+  document.getElementById("title").innerHTML = "LUNCH!";
+  document.getElementById("pick").style.display = "none";
+  document.getElementById("place").innerHTML =
+    randomRestaurant[0] +
+    '<br><a href="https://maps.google.com/?q=' +
+    randomRestaurant[1] +
+    '">' +
+    randomRestaurant[1] +
+    "</a>";
+}
