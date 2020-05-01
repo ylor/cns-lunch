e6f476a966e276ae690fbce479baf1a75ca681e0
ylor <rolyreyes@me.com>
2020-05-01 16:42:41 -0400

    actually this time though

--- randomizer.js
+++ randomizer.js
@@ -35,8 +35,8 @@ var randomRestaurant = array[(Math.random() * array.length) | 0];
 function buttonFunction() {
   document.getElementById("title").innerHTML = "LUNCH!";
   document.getElementById("pick").style.display = "none";
-  //document.getElementById("place").innerHTML =
-  //randomRestaurant[0] +
+  document.getElementById("place").innerHTML =
+  randomRestaurant[0] +
   //'<br><a href="https://maps.google.com/?q=' +
   //randomRestaurant[1] +
   //'">' +
