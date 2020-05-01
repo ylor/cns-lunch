afc82c3859771865af169225e78e35cbf23d2e5c
ylor <rolyreyes@me.com>
2020-05-01 16:41:57 -0400

    remove dummy address for now

--- randomizer.js
+++ randomizer.js
@@ -27,7 +27,7 @@ var array = [
   ["🍣", "11625 N Kendall Dr, Miami, FL 33176"],
   ["🍤", "11625 N Kendall Dr, Miami, FL 33176"],
   ["🥟", "11625 N Kendall Dr, Miami, FL 33176"],
-  ["🥡", "11625 N Kendall Dr, Miami, FL 33176"]
+  ["🥡", "11625 N Kendall Dr, Miami, FL 33176"],
 ];
 
 var randomRestaurant = array[(Math.random() * array.length) | 0];
@@ -35,11 +35,11 @@ var randomRestaurant = array[(Math.random() * array.length) | 0];
 function buttonFunction() {
   document.getElementById("title").innerHTML = "LUNCH!";
   document.getElementById("pick").style.display = "none";
-  document.getElementById("place").innerHTML =
-    randomRestaurant[0] +
-    '<br><a href="https://maps.google.com/?q=' +
-    randomRestaurant[1] +
-    '">' +
-    randomRestaurant[1] +
-    "</a>";
+  //document.getElementById("place").innerHTML =
+  //randomRestaurant[0] +
+  //'<br><a href="https://maps.google.com/?q=' +
+  //randomRestaurant[1] +
+  //'">' +
+  //randomRestaurant[1] +
+  //"</a>";
 }
